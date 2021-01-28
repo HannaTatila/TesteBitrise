bb338a01100ef774c52185a8d16179b174cf1d0d
hannasousa <hanna.tati@gmail.com>
2021-01-28 02:19:17 -0300

    Ajuste retrofit

--- app/src/main/java/com/example/testebitrise/di/ComicsModule.kt
+++ app/src/main/java/com/example/testebitrise/di/ComicsModule.kt
@@ -9,101 +9,21 @@ import com.example.testebitrise.domain.repository.ComicRepository
 import com.example.testebitrise.domain.usecase.GetComicUseCase
 import com.example.testebitrise.domain.usecase.GetComicUseCaseImpl
 import com.example.testebitrise.presentation.ListComicsViewModel
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ComicsModule {
 
-//    private val gson: Gson by lazy { GsonBuilder().create() }
-//
-//    private val okHttp: OkHttpClient by lazy {
-//        OkHttpClient.Builder()
-//            .build()
-//    }
-//
-//    private val retrofit: Retrofit by lazy {
-//        Retrofit.Builder()
-//            .baseUrl("https://gateway.marvel.com/")
-//            .client(okHttp)
-//            .addConverterFactory(GsonConverterFactory.create(gson))
-//            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-//            .build()
-//    }
-//
-//    private val comicsService: ComicService by lazy {
-//        retrofit.create(ComicService::class.java)
-//    }
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://gateway.marvel.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .client(OkHttpClient.Builder().build())
+        .build()
 
-//    private val logging = HttpLoggingInterceptor().apply {
-//        this.level = HttpLoggingInterceptor.Level.BODY
-//    }
-//
-//    var httpClient: OkHttpClient.Builder = OkHttpClient.Builder().apply {
-//        this.addInterceptor(logging)
-//    }
-//
-//    private val retrofit = Retrofit.Builder()
-//        .baseUrl("https://gateway.marvel.com/")
-//        .addConverterFactory(GsonConverterFactory.create())
-//        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-//        .client(httpClient.build())
-//        .build()
-
-//    private val comicsService = retrofit.create(ComicService::class.java)
-
-//-----------------
-
-//    private val url = "https://gateway.marvel.com/"
-//
-//    private val gson: Gson by lazy { GsonBuilder().create() }
-//
-//    private val okHttp: OkHttpClient by lazy {
-//        OkHttpClient.Builder()
-//            .build()
-//    }
-//
-//    private val retrofit: Retrofit by lazy {
-//        Retrofit.Builder()
-//            .baseUrl(url)
-//            .client(okHttp)
-//            .addConverterFactory(GsonConverterFactory.create(gson))
-//            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-//            .build()
-//    }
-//
-//    private val comicsService: ComicService by lazy {
-//        retrofit.create(ComicService::class.java)
-//    }
-
-//    -------------
-
-    private val url = "https://gateway.marvel.com/"
-
-    private val gson: Gson by lazy { GsonBuilder().create() }
-
-    private val okHttp: OkHttpClient by lazy {
-        OkHttpClient.Builder()
-            .build()
-    }
-
-    private val retrofit: Retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl(url)
-            .client(okHttp)
-            .addConverterFactory(GsonConverterFactory.create(gson))
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-            .build()
-    }
-
-    private val comicsService: ComicService by lazy {
-        retrofit.create(ComicService::class.java)
-    }
-
+    private val comicsService = retrofit.create(ComicService::class.java)
     private val dataSource: ComicRemoteDataSource = ComicRemoteDataSourceImpl(comicsService)
     private val mapper: ComicMapper = ComicMapper()
     private val repository: ComicRepository = ComicRepositoryImpl(dataSource, mapper)
